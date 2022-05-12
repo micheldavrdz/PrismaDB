dc3652094d19785dbe854879e02ac29b18932fac
Michel <micheldavrdz@gmail.com>
2022-05-12 18:12:39 -0500

    update: seed.js explorer_2

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,67 +3,43 @@ const prisma = new PrismaClient();
 
 (async function main() {
   try {
-    const woopa = await prisma.explorer.upsert({
+    const woopa = await prisma.explorer_2.upsert({
       where: { name: 'Woopa' },
       update: {},
       create: {
         name: 'Woopa',
-				username: 'ajolonauta',
-				mission: 'Node'
+		lang: 'Python',
+        missionCommander: 'Carlo',
+        enrollments: 5,
+		hasCertifications: true
       },
     });
 
-    const woopa1 = await prisma.explorer.upsert({
+    const woopa1 = await prisma.explorer_2.upsert({
       where: { name: 'Woopa1' },
       update: {},
       create: {
         name: 'Woopa1',
-				username: 'ajolonauta1',
-				mission: 'Node'
+		lang: 'Javascript',
+        missionCommander: 'Carlo',
+        enrollments: 1,
+		hasCertifications: true
       },
     });
 
-    const woopa2 = await prisma.explorer.upsert({
+    const woopa2 = await prisma.explorer_2.upsert({
       where: { name: 'Woopa 2' },
       update: {},
       create: {
-        name: 'Woopa 2',
-				username: 'ajolonauta2',
-				mission: 'Java'
+        name: 'Woopa2',
+		lang: 'Javascript',
+        missionCommander: 'Carlo',
+        enrollments: 3,
+		hasCertifications: false
       },
     });
 
-    const woopa3 = await prisma.explorer.upsert({
-      where: { name: 'Woopa 3' },
-      update: {},
-      create: {
-        name: 'Woopa 3',
-				username: 'ajolonauta3',
-				mission: 'Node'
-      },
-    });
-
-    const woopa4 = await prisma.explorer.upsert({
-        where: { name: 'Woopa 4' },
-        update: {},
-        create: {
-          name: 'Woopa 4',
-                  username: 'ajolonauta4',
-                  mission: 'Node'
-        },
-    });
-
-    const woopa5 = await prisma.explorer.upsert({
-        where: { name: 'Woopa 5' },
-        update: {},
-        create: {
-          name: 'Woopa 5',
-                  username: 'ajolonauta5',
-                  mission: 'Node'
-        },
-    });
-
-    console.log('Create 5 explorers');
+    console.log('Create 3 explorers');
   } catch(e) {
     console.error(e);
     process.exit(1);
