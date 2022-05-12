e0908a9e78bffd48de2215b8ef7b1d88bc996c53
Michel <micheldavrdz@gmail.com>
2022-05-12 18:27:08 -0500

    add: endpoints for explorer_2 (sames as normal explorer)

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.get('/', (req, res) => {
   res.json({message: 'alive'});
 });
 
+// Explorer TABLE Endpoints
 app.get('/explorers', async (req, res) => {
     const allExplorers =  await prisma.explorer.findMany({});
     res.json(allExplorers);
@@ -51,6 +52,52 @@ app.put('/explorers/:id', async (req, res) => {
 app.delete('/explorers/:id', async (req, res) => {
 	const id = parseInt(req.params.id);
 	await prisma.explorer.delete({where: {id: id}});
+	return res.json({message: "Eliminado correctamente"});
+});
+
+// Explorer_2 TABLE Endpoints
+app.get('/explorers2', async (req, res) => {
+    const allExplorers =  await prisma.explorer_2.findMany({});
+    res.json(allExplorers);
+});
+
+app.get('/explorers2/:id', async (req, res) => {
+    const id = req.params.id;
+    const explorer = await prisma.explorer_2.findUnique({where: {id: parseInt(id)}});
+    res.json(explorer);
+});
+
+app.post('/explorers2', async (req, res) => {
+    const explorer = {
+      name: req.body.name,
+      lang: req.body.lang,
+      missionCommander: req.body.missionCommander,
+      enrollments: req.body.enrollments,
+      hasCertifications: req.body.hasCertifications
+     };
+    const message = 'Explorer creado.';
+    await prisma.explorer_2.create({data: explorer});
+    return res.json({message});
+});
+
+app.put('/explorers2/:id', async (req, res) => {
+	const id = parseInt(req.params.id);
+
+	await prisma.explorer_2.update({
+		where: {
+			id: id
+		},
+		data: {
+			hasCertifications: req.body.hasCertifications
+		}
+	})
+
+	return res.json({message: "Actualizado correctamente"});
+});
+
+app.delete('/explorers2/:id', async (req, res) => {
+	const id = parseInt(req.params.id);
+	await prisma.explorer_2.delete({where: {id: id}});
 	return res.json({message: "Eliminado correctamente"});
 });
 
